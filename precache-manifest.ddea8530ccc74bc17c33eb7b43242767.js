021860ea2d9ccd78a9aaa59510d23d879603c903
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-07-19 15:30:55 +0530

    Updates

---
+++ precache-manifest.ddea8530ccc74bc17c33eb7b43242767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ba9ccb86e742d97034c192c9191402",
+    "url": "/myschedule/index.html"
+  },
+  {
+    "revision": "16ae7fec4c54ce1ad32d",
+    "url": "/myschedule/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/myschedule/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "16ae7fec4c54ce1ad32d",
+    "url": "/myschedule/static/js/main.f5d29bc8.chunk.js"
+  },
+  {
+    "revision": "ed76a72e39873399cff8",
+    "url": "/myschedule/static/js/runtime~main.113e0055.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myschedule/static/media/logo.5d5d9eef.svg"
+  }
+]);
